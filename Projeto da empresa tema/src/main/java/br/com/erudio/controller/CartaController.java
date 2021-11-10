a963d348b9abcf730f8b39e51106fdc18ecdbd39
Paulo-dev-98 <paulobg8@gmail.com>
2021-11-10 17:24:57 -0300

    implementando ordenacao

--- Projeto da empresa tema/src/main/java/br/com/erudio/controller/CartaController.java
+++ Projeto da empresa tema/src/main/java/br/com/erudio/controller/CartaController.java
@@ -8,6 +8,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,10 +37,13 @@ public class CartaController {
 
 	@ApiOperation(value = "busca todas as cartas")
 	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
-	public List<CartaVO> findAll(@RequestParam(value="page", defaultValue = "0") int page,
-			@RequestParam(value="limit", defaultValue = "5") int limit) {
+	public List<CartaVO> findAll(
+			@RequestParam(value="page", defaultValue = "0") int page,
+			@RequestParam(value="limit", defaultValue = "5") int limit,
+			@RequestParam(value="direction", defaultValue = "asc") String direction) {
 		
-		Pageable pageable = PageRequest.of(page, limit);
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection,"nome"));
 		
 		List<CartaVO> cartas = service.findAll(pageable);
 		cartas.stream()
